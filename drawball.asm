54f4e221ae1f14e912081491dc3a51e3645bc643
Filip Wiśniewski <01149976@pw.edu.pl>
2021-06-16 01:38:56 +0200

    Final verson before review

--- drawball.asm
+++ drawball.asm
@@ -1,19 +1,170 @@
-        section .data
-    
-        section .text
-        global  main
-        extern drawball
-drawball:
-;prolog
-        push    rbp             ;zapamiętanie wskaźnika ramki procedury wywołującej
-        mov     rbp, rsp        ;ustanowienie własnego wskaźnika ramki
+; ----------------------------------------------------
+;
+; arguments passed
+; ----------------
+; rdi   -   bitmap pointer (top left pixel)
+; rsi   -   width including pixel size (32 bit)
+; rdx   -   height
+; rcx   -   x click position
+; r8    -   y click position
+; xmm0  -   initial velocity (float)
+;
+;
+; local values
+; ------------
+; r10d  -   drawing line color
+; r11   -   cur_x counter
+; r12   -   calculated y coordinate
+; xmm?  -   described next to use case (often temporary)
+;
+; ---------------------------------------------------
 
-;ciało procedury
-        mov     rax, 0x1        ;system call for write
-        mov     rdx, rdi        ;string size
-        mov     rdi, 0x1        ;file descriptor (1 - std out)
-        syscall
+section .text
+global  drawball
+
+drawball:
+
+; ------
+; prolog
+; ------
+        push    rbp
+        mov     rbp, rsp
+
+        push    rbx                
+	push    r12
+	push    r13
+	push    r14
+	push    r15
+
+
+        ; initialise values
+        ; ----------------
+        mov     r11, 0                  ;r11  - cur_x counter
+        mov     r10d, 0x0000ffff        ;r10d - drawing line color in format RRGGBBAA
+
+
+        ; revert y coordinate
+        ; -------------------
+        mov     r9, rdx
+        sub     r9, r8
+        mov     r8, r9
+
+
+        ; move bitmap beginning to bottom-left pixel
+        ; ------------------------------------------
+        mov     rax, rdx
+        mul     rsi                     ;rax = rsi * rdx (pitch * height)
+        mov     r15, rdi                ;save original rdi
+        sub     rdi, rax
+        
+
+ draw:  ; draw shot line on passed bitmap
+        ; -------------------------------
+        
+
+        ; count y(cur_x) = tanß*cur_x - (cur_x^2*g)/(2*v_0^2*cos^2ß)
+        ; ---------------------------------------------------------
+        ; r11 - current x index
+        ; rsi - width
+        ; rcx - x click position
+        ; r8  - y click position
+        
+        ; count tanß*cur_x
+        cvtsi2ss        xmm1, r11               ;xmm1 - current x value
+        cvtsi2ss        xmm3, rcx               ;xmm3 - mouse x
+        cvtsi2ss        xmm4, r8                ;xmm4 - mouse y
+        divss           xmm4, xmm3              ;xmm4 - y/x = tanß
+        mulss           xmm4, xmm1              ; -> xmm4 - tanß*cur_x
+
+        ;count cos^2ß
+        cvtsi2ss        xmm2, rcx               ;xmm2 - mouse x
+        cvtsi2ss        xmm3, r8                ;xmm2 - mouse y
+        mulss           xmm2, xmm2              ;xmm2 - x^2
+        mulss           xmm3, xmm3              ;xmm3 - y^2
+        addss           xmm3, xmm2              ;xmm3 - x^2 + y^2
+        sqrtss          xmm3, xmm3              ;xmm3 - distance between x and y (c)
+        cvtsi2ss        xmm2, rcx               ;xmm2 - mouse x
+        divss           xmm2, xmm3              ;xmm2 - cosß
+        mulss           xmm2, xmm2              ; -> xmm2 - cos^2ß
+
+        ;count 2*v_0^2
+        movss           xmm5, xmm0              ;copy of xmm0
+        mulss           xmm5, xmm5              ;xmm5 - v_0^2
+        addss           xmm5, xmm5              ;-> xmm5 - 2*v_0^2
+
+        ;count cur_x^2*g, g=8
+        mov             r12, 0x1000
+        cvtsi2ss        xmm3, r12               ;xmm3 - g
+        cvtsi2ss        xmm1, r11               ;xmm1 - cur_x
+        mulss           xmm1, xmm1              ;xmm1 - cur_x^2
+        mulss           xmm1, xmm3              ; -> xmm1 - cur_x^2 * g --?
+
+        ;count all (final)
+        mulss           xmm5, xmm2              ;xmm5 - (2*v_o^2*cos^2ß)
+        divss           xmm1, xmm5              ;xmm1 - (cur_x^2*g)/(2*v_o^2*cos^2ß)
+        subss           xmm4, xmm1              ; -> xmm4 - y(cur_x)
+
+        ;convert fp value to integer
+        cvttss2si       r12, xmm4               ; -> r12  - y coordinate corresponding to cur_x
+
+
+        ; prevent painting behind the bottom edge
+        ; ---------------------------------------
+        cmp             r12, 0
+        jle             skip_painting
+
+
+        ; find draw index based on y calculated
+        ; -------------------------------------
+        ; r12 - current y index
+        ; r11 - current x index
+        ; rdi - current x pointer
+        ; rsi - width
+
+        mov     r13, rdi        ;r13 - copy of rdi
+        mov     rax, rsi        ;rax - width
+        mul     r12             ;rax - padding of size of width*y
+        add     r13, rax        ; -> r13 - place to paint pixel
+
+
+        ; prevent painting behind the upper edge
+        ;---------------------------------------
+        cmp     r13, r15
+        jge     skip_painting
+
+
+        ; paint pixel found
+        ; -----------------
+        mov     [r13], r10d     ;color
+
+
+skip_painting: 
+
+        ; increment cur_x
+        ; ---------------
+        add     rdi, 4
+        inc     r11
+
+        ; prevent painting behind the right edge
+        ; --------------------------------------
+        mov     r14, rsi
+        shr     r14, 2          ;divide by 4
+        sub     r14, r11
+        cmp     r14, 0
+        jge     draw
+
+
+        ; procedure end
+        ; -------------
+
 
 ; epilog
+; ------
+        pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+
         pop     rbp
         ret
--- main.c
+++ main.c
@@ -4,50 +4,107 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void drawball(char size, char* string);
+
+#define WIDTH   800
+#define HEIGHT  600
+#define V_STEP  100
+#define V_START 1700
+
+
+void drawball(void* pixel_buffer, int width, int height, int x, int y, float V);
+// draws throw graph on pixel_buffer passed based on initial velocity
+
 
 int main() 
 {
-    char* string = "duuuża kupa kurde\n";
-    drawball(strlen(string), string);
+    bool done = false;
+    float V = V_START;
+    char* V_stringed = malloc(5);
 
+    // initialise Allegro components
     al_init();
     al_install_mouse();
     al_install_keyboard();
 
-    ALLEGRO_DISPLAY* disp = al_create_display(800, 600);
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_FONT* font = al_create_builtin_font();
     ALLEGRO_EVENT event;
+    ALLEGRO_MOUSE_STATE mouse_state;
+    ALLEGRO_DISPLAY* disp = al_create_display(WIDTH, HEIGHT);
+    ALLEGRO_BITMAP* bmp = al_create_bitmap(WIDTH, HEIGHT);
+    ALLEGRO_LOCKED_REGION* region;
+    ALLEGRO_FONT* font = al_create_builtin_font();
 
-    al_register_event_source(queue, al_get_keyboard_event_source());
+    // initialise event sources
     al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_mouse_event_source());
+    al_register_event_source(queue, al_get_keyboard_event_source());
 
-    int i = 0;
-    char* i_str = malloc(5);
+    // clear initial view 
 
-    while(1)
-    {
-        // allegro procedures and assembly
-        al_clear_to_color(al_map_rgb(255, 255, 255));
-        al_draw_text(font, al_map_rgb(0, 0, 0), 0, 0, 0, i_str);
-        al_flip_display();
+    al_draw_text(font, al_map_rgb(255, 255, 255), (WIDTH/2-50), HEIGHT/2, 0, "Click to start");
+    al_flip_display();
+
+    // main loop
+    while(!done)
+    {   
         al_wait_for_event(queue, &event);
-        if (event.type == ALLEGRO_KEY_BACK || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+
+        switch (event.type)
         {
+        case ALLEGRO_EVENT_KEY_DOWN:
+            if(event.keyboard.keycode == ALLEGRO_KEY_UP)
+            {
+                V += V_STEP;
+            }
+            else if(event.keyboard.keycode == ALLEGRO_KEY_DOWN)
+            {
+                if (V >= V_STEP ) { V -= V_STEP; }
+            }
+            // no break here in order to refresh screen from next case
+
+        case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+
+            // get mouse state
+            al_get_mouse_state(&mouse_state);
+
+            // clear bitmap
+            al_set_target_bitmap(bmp);
+            al_clear_to_color(al_map_rgb(255, 255, 255));
+
+            // clear display
+            al_set_target_backbuffer(disp);
+            al_clear_to_color(al_map_rgb(255, 255, 255));
+
+            // lock and modify bitmap
+            region = al_lock_bitmap(bmp, ALLEGRO_PIXEL_FORMAT_RGBA_8888, ALLEGRO_LOCK_READWRITE);
+            drawball(region->data, WIDTH*4, HEIGHT, mouse_state.x, mouse_state.y, V);
+            al_unlock_bitmap(bmp);
+
+            // draw updated bitmap
+            al_draw_bitmap(bmp, 0, 0, 0);
+
+            // draw updated text
+            al_draw_text(font, al_map_rgb(0, 0, 0), 0, 0, 0, "Initial velocity: ");
+            sprintf(V_stringed, "%f", V);
+            al_draw_text(font, al_map_rgb(0, 0, 0), 200, 0, 0, V_stringed);
+
+            // display
+            al_flip_display();
+
             break;
-        }
-        else
-        {
-            i++;
-            sprintf(i_str, "%d", i);
+
+        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+            done = true;
+            break;
         }
     }
 
-    free(i_str);
-    al_destroy_font(font);
+    // deallocate memory
     al_destroy_display(disp);
     al_destroy_event_queue(queue);
+    al_destroy_bitmap(bmp);
+    al_destroy_font(font);
+    free(V_stringed);
 
     return 0;
 }
